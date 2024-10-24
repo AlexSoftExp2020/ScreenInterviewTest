cfe83ca591d534c304923dabeefa292cf3e7d5d2
Alex <oleksii.pidhornyi@gmail.com>
2024-10-24 20:51:54 +0200

    Initial Commit

---
+++ ScreenInterviewTest/ScreenInterviewTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenInterviewTestApp.swift
+//  ScreenInterviewTest
+//
+//  Created by Oleksii on 24.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct ScreenInterviewTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
